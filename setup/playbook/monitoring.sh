f140aa9b7db863866af10e2d8507030f0184025d
Feng Xue <xfgavin@gmail.com>
2020-10-12 12:08:48 -0700

    Updated playbooks

--- setup/playbook/monitoring.sh
+++ setup/playbook/monitoring.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 sed -e "s/AWSUSER/$AWS_USER/g" monitoring.yml > monitoring_tmp.yml
+sed -e "s/DBUSER/$DBUSER/g" -e "s/DBPWD/$DBPWD/g" docker-compose_postgis.yml > docker-compose_postgis_tmp.yml
 ansible-playbook -i inventory.yml --private-key=$AWS_SSH_KEY -u $AWS_USER -f 10 monitoring_tmp.yml
 rm -rf monitoring_tmp.yml
+rm -rf docker-compose_postgis_tmp.yml
---
+++ setup/playbook/stop_monitor.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /opt/www/status.dtrace.net
+docker-compose down
